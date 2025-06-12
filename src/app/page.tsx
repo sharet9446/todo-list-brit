8869efe1377ad36b69cea02bba2d92761756deea
sharet9446 <spees97@naver.com>
2025-06-12 18:40:23 +0900

    feat: Home 컴포넌트에 TodoList 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import TodoForm from "@/components/TodoForm";
+import TodoList from "@/components/TodoList";
 
 export default function Home() {
-  return <TodoForm />;
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
 }
